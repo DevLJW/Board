33a03cfc97b5530b14e4c441b3f700cf9644de47
DevLJW <jaewookv123@gmail.com>
2023-02-22 20:04:47 +0900

    JSX add

--- freeboard/pages/boards/[BoardId]/index.js
+++ freeboard/pages/boards/[BoardId]/index.js
@@ -1,5 +1,19 @@
 import { useRouter } from "next/router";
-import { Wrapper } from "../../../styles/BoardDetail";
+import {
+  Avatar,
+  AvatarWrapper,
+  Body,
+  BottomWrapper,
+  Button,
+  CardWrapper,
+  Contents,
+  CreatedAt,
+  Header,
+  Info,
+  Title,
+  Wrapper,
+  Writer,
+} from "../../../styles/BoardDetail";
 import { gql, useQuery } from "@apollo/client";
 
 const BOARD_DETAIL_QURIES = gql`
@@ -24,5 +38,28 @@ export default function BoardDetailPage() {
 
   console.log(data);
 
-  return <Wrapper>123</Wrapper>;
+  return (
+    <Wrapper>
+      <CardWrapper>
+        <Header>
+          <AvatarWrapper>
+            <Avatar src="/images/avatar.png" />
+            <Info>
+              <Writer>{data?.fetchBoard?.writer}</Writer>
+              <CreatedAt>{data?.fetchBoard?.createdAt}</CreatedAt>
+            </Info>
+          </AvatarWrapper>
+        </Header>
+        <Body>
+          <Title>{data?.fetchBoard?.title}</Title>
+          <Contents>{data?.fetchBoard?.contents}</Contents>
+        </Body>
+      </CardWrapper>
+      <BottomWrapper>
+        <Button>목록으로</Button>
+        <Button>수정하기</Button>
+        <Button>삭제하기</Button>
+      </BottomWrapper>
+    </Wrapper>
+  );
 }
