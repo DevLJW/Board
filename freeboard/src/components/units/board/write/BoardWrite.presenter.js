2d93f1e78dd4508fc3d41c9d98650aa4c4eacfbd
DevLJW <jaewookv123@gmail.com>
2023-02-25 18:56:49 +0900

    BoardDetail BoardWrite Component/Presenter add

---
+++ freeboard/src/components/units/board/write/BoardWrite.presenter.js
@@ -0,0 +1,77 @@
+import * as W from "./BoardWrite.styles";
+
+export default function BoardWriteUI(props) {
+  return (
+    <W.Wrapper>
+      <W.Title>게시글 등록</W.Title>
+      <W.WriterWrapper>
+        <W.InputWrapper>
+          <W.Label>작성자</W.Label>
+
+          <W.Writer
+            type="text"
+            placeholder="이름을 입력 해주세요."
+            onChange={props.onChangeTitle}
+          ></W.Writer>
+          <W.Error>{props.writerError}</W.Error>
+        </W.InputWrapper>
+        <W.InputWrapper>
+          <W.Label>비밀번호</W.Label>
+          <W.Writer
+            type="password"
+            placeholder="패스워드를 입력 해주세요."
+            onChange={props.onChangePassword}
+          ></W.Writer>
+          <W.Error>{props.passwordError}</W.Error>
+        </W.InputWrapper>
+      </W.WriterWrapper>
+      <W.InputWrapper>
+        <W.Label>제목</W.Label>
+        <W.Subject
+          type="text"
+          placeholder="제목을 작성해주세요."
+          onChange={props.onChangeTitle}
+        />
+        <W.Error>{props.titleError1}</W.Error>
+      </W.InputWrapper>
+      <W.InputWrapper>
+        <W.Label>내용</W.Label>
+        <W.Content
+          placeholder="내용을 작성 해주세요."
+          onChange={props.onChangeContents}
+        />
+        <W.Error>{props.contentsError}</W.Error>
+      </W.InputWrapper>
+
+      <W.InputWrapper>
+        <W.Label>주소</W.Label>
+        <W.ZipcodeWrapper>
+          <W.Zipcode placeholder="07250"></W.Zipcode>
+          <W.SearchButton>우편번호검색</W.SearchButton>
+        </W.ZipcodeWrapper>
+        <W.Address></W.Address>
+        <W.Address></W.Address>
+      </W.InputWrapper>
+      <W.InputWrapper>
+        <W.Label>유튜브</W.Label>
+        <W.Youtube placeholder="링크를 복사해주세요." />
+      </W.InputWrapper>
+      <W.ImageWrapper>
+        <W.UploadButton>+</W.UploadButton>
+        <W.UploadButton>+</W.UploadButton>
+        <W.UploadButton>+</W.UploadButton>
+      </W.ImageWrapper>
+
+      <W.OptionWrapper>
+        <W.Label>메인설정</W.Label>
+        <W.RadioButton type="radio" id="youtube" name="radio-button" />
+        <W.RadioLabel htmlFor="youtube">유튜브</W.RadioLabel>
+        <W.RadioButton type="radio" id="image" name="radio-button" />
+        <W.RadioLabel htmlFor="image">사진</W.RadioLabel>
+      </W.OptionWrapper>
+      <W.ButtonWrapper>
+        <W.SubmitButton onClick={props.onClickSubmit}>등록</W.SubmitButton>
+      </W.ButtonWrapper>
+    </W.Wrapper>
+  );
+}
---
+++ freeboard/src/components/units/board/write/BoardWriter.container.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import BoardWriteUI from "./BoardWrite.presenter";
+import { CREATE_BOARD } from "./BoardWrite.quries";
+
+export default function BoardWrite() {
+  const [writer, setWriter] = useState("");
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [writerError, setWriterError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [contentsError, setContentsError] = useState("");
+
+  const [BoardInfoAdd] = useMutation(CREATE_BOARD); //API 통신 함수
+
+  const router = useRouter();
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+    if (event.target.value !== "") {
+      //빈칸에서 버튼 클릭시 ,에러 메시지 출력 --> 내용입력시 에러메시지 없애주는 용도
+      setWriterError("");
+    }
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value !== "") {
+      setPasswordError(""); //에러 초기화
+    }
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+    if (event.target.value !== "") {
+      setTitleError("");
+    }
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+    if (event.target.value !== "") {
+      setContentsError("");
+    }
+  };
+
+  const onClickSubmit = async () => {
+    if (!writer) {
+      setWriterError("작성자를 입력해주세요.");
+    }
+    if (!password) {
+      setPasswordError("비밀번호를 입력해주세요.");
+    }
+    if (!title) {
+      setTitleError("제목을 입력 해주세요.");
+    }
+    if (!contents) {
+      setContentsError("내용을 입력해주세요.");
+    }
+    if (writer && password && title && contents) {
+      alert("게시글이 등록 되었습니다.");
+    }
+
+    try {
+      const result = await BoardInfoAdd({
+        variables: {
+          createBoardInput: {
+            writer: writer,
+            password: password,
+            title: title,
+            contents: contents,
+          },
+        },
+      });
+      //console.log(result);
+      router.push(`/boards/${result.data.createBoard._id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <BoardWriteUI
+      writerError={writerError}
+      passwordError={passwordError}
+      titleError1={titleError}
+      contentsError={contentsError}
+      onChangeWriter={onChangeWriter}
+      onChangePassword={onChangePassword}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+      onClickSubmit={onClickSubmit}
+    />
+  );
+}
