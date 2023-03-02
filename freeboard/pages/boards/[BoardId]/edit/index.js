04d5118d20fd0e3cf00da22016b2025d513d5d32
DevLJW <jaewookv123@gmail.com>
2023-03-02 11:03:34 +0900

    isEdit and Queries add

--- freeboard/pages/boards/[BoardId]/edit/index.js
+++ freeboard/pages/boards/[BoardId]/edit/index.js
@@ -1,5 +1,5 @@
 import BoardWrite from "@/src/components/units/board/write/BoardWriter.container";
-//등록페이지
+//수정 페이지
 export default function BoardWritePage() {
-  return <BoardWrite isEdit={false} />;
+  return <BoardWrite isEdit={true} />;
 }
--- freeboard/src/components/units/board/write/BoardWriter.container.js
+++ freeboard/src/components/units/board/write/BoardWriter.container.js
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
 import BoardWriteUI from "./BoardWrite.presenter";
-import { CREATE_BOARD } from "./BoardWrite.quries";
+import { CREATE_BOARD, UPDATE_BOARD } from "./BoardWrite.quries";
 
-export default function BoardWrite() {
+export default function BoardWrite(props) {
   const [writer, setWriter] = useState("");
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
+  const [isActive, setIsActive] = useState("false"); //버튼 활성화 훅
 
   const [writerError, setWriterError] = useState("");
   const [passwordError, setPasswordError] = useState("");
@@ -16,6 +17,7 @@ export default function BoardWrite() {
   const [contentsError, setContentsError] = useState("");
 
   const [BoardInfoAdd] = useMutation(CREATE_BOARD); //API 통신 함수
+  const [updateBoard] = useMutation(UPDATE_BOARD);
 
   const router = useRouter();
 
@@ -24,6 +26,12 @@ export default function BoardWrite() {
     if (event.target.value !== "") {
       //빈칸에서 버튼 클릭시 ,에러 메시지 출력 --> 내용입력시 에러메시지 없애주는 용도
       setWriterError("");
+    }
+
+    if (event.target.value && password && title && contents) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
     }
   };
 
@@ -83,6 +91,24 @@ export default function BoardWrite() {
     }
   };
 
+  const onClickUpdate = async () => {
+    try {
+      const result = await updateBoard({
+        variables: {
+          boardId: router.query.boardId,
+          password: password,
+          updateBoardInput: {
+            title: title,
+            contents: contents,
+          },
+        },
+      });
+      router.push(`/boards/${result.data.updateBoard._id}`);
+    } catch (error) {
+      console.log(error.log);
+    }
+  };
+
   return (
     <BoardWriteUI
       writerError={writerError}
@@ -94,6 +120,8 @@ export default function BoardWrite() {
       onChangeTitle={onChangeTitle}
       onChangeContents={onChangeContents}
       onClickSubmit={onClickSubmit}
+      onClickUpdate={onClickUpdate}
+      isEdit={props.isEdit}
     />
   );
 }
