e131b1efff91a50ccbc7e426d9a0316039db6449
DevLJW <jaewookv123@gmail.com>
2023-03-04 23:57:13 +0900

    board comment add

--- freeboard/src/components/units/boardComment/write/BoardCommentWrite.container.js
+++ freeboard/src/components/units/boardComment/write/BoardCommentWrite.container.js
@@ -1,10 +1,68 @@
 import { useMutation } from "@apollo/client";
+import { useState } from "react";
 import BoardCommentUI from "./BoardCommentWrite.presenter";
+import { CREATE_BOARD_COMMENT } from "./BoardComment.quries";
+import { useRouter } from "next/router";
+import { FETCH_BOARD_COMMENTS } from "../list/BoardCommentListqueries";
 
 export default function BoardCommentWrite() {
-  const [Writer, setWriter] = useMutation("");
-  const [PassWord, setPassWord] = useMutation("");
-  const [Contents, setContents] = useMutation("");
+  const router = useRouter();
+  const [Writer, setWriter] = useState("");
+  const [PassWord, setPassWord] = useState("");
+  const [Contents, setContents] = useState("");
+  const [createBoardComment] = useMutation(CREATE_BOARD_COMMENT);
 
-  return <BoardCommentUI />;
+  const onChangeWriter = (event) => {
+    //작성자 텍스트란 변경감지 함수
+    setWriter(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    //패스워드 텍스트란 변경감지 함수
+    setPassWord(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    //패스워드 텍스트란 변경감지 함수
+    setContents(event.target.value);
+  };
+
+  const onClickWrite = async () => {
+    //등록하기 버튼
+
+    await createBoardComment({
+      variables: {
+        createBoardCommentInput: {
+          writer: Writer,
+          password: PassWord,
+          contents: Contents,
+          rating: 0,
+        },
+        boardId: router.query.boardId,
+      },
+
+      refetchQueries: [
+        {
+          query: FETCH_BOARD_COMMENTS,
+          variables: {
+            boardId: router.query.boardId,
+          },
+        },
+      ],
+    });
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <BoardCommentUI
+      onChangeWriter={onChangeWriter}
+      onChangePassword={onChangePassword}
+      onChangeContents={onChangeContents}
+      onClickWrite={onClickWrite}
+      Contents={Contents}
+    />
+  );
 }
--- freeboard/src/components/units/boardComment/write/BoardCommentWrite.presenter.js
+++ freeboard/src/components/units/boardComment/write/BoardCommentWrite.presenter.js
@@ -9,21 +9,30 @@ export default function BoardCommentUI(props) {
       </>
 
       <S.InputWrapper>
-        <S.Input type="text" placeholder="작성자"></S.Input>
-        <S.Input type="password" placeholder="비밀번호"></S.Input>
+        <S.Input
+          type="text"
+          placeholder="작성자"
+          onChange={props.onChangeWriter}
+        ></S.Input>
+        <S.Input
+          type="password"
+          placeholder="비밀번호"
+          onChange={props.onChangePassword}
+        ></S.Input>
       </S.InputWrapper>
 
       <S.ContentsWrapper>
         <S.Contents
+          onChange={props.onChangeContents}
           maxLength={100}
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
         ></S.Contents>
-      </S.ContentsWrapper>
 
-      <S.BottomWrapper>
-        <S.ContentsLength>/100</S.ContentsLength>
-        <S.Button onClick={props.onClickWrite}>등록하기</S.Button>
-      </S.BottomWrapper>
+        <S.BottomWrapper>
+          <S.ContentsLength>{props.Contents.length}/100</S.ContentsLength>
+          <S.Button onClick={props.onClickWrite}>등록하기</S.Button>
+        </S.BottomWrapper>
+      </S.ContentsWrapper>
     </S.Wrapper>
   );
 }
