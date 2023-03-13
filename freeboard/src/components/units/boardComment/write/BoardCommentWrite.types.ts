1fe7b4a9a37e1385367382ee49c6a253694bda4f
DevLJW <jaewookv123@gmail.com>
2023-03-13 13:50:15 +0900

    comment Rate(star) add

--- freeboard/src/components/units/boardComment/write/BoardCommentWrite.types.ts
+++ freeboard/src/components/units/boardComment/write/BoardCommentWrite.types.ts
@@ -1,9 +1,10 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
 export interface IBoardCommentWriteUIProps {
-  onChangeWriter: (event: ChangeEvent<HTMLInputElement>) => void;
-  onChangePassword: (event: ChangeEvent<HTMLInputElement>) => void;
-  onChangeContents: (event: ChangeEvent<HTMLTextAreaElement>) => void;
-  onClickWrite: () => void;
-  Contents: string;
+      onChangeWriter: (event: ChangeEvent<HTMLInputElement>) => void;
+      onChangePassword: (event: ChangeEvent<HTMLInputElement>) => void;
+      onChangeContents: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+      onClickWrite: () => void;
+      Contents: string;
+      setStar: Dispatch<SetStateAction<number>>;
 }
