3987fb14e93bc2ff337c925fd8aaf32a321b41de
DevLJW <jaewookv123@gmail.com>
2023-03-17 14:20:14 +0900

    Common Layout add

---
+++ freeboard/src/commons/layout/index.tsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+import LayoutBanner from "./banner/LayoutBanner.container";
+import LayoutHeader from "./header/LayoutHeader.container";
+import LayoutNavigation from "./navigation/LayoutNavigation.container";
+
+interface ILayoutProps {
+  children: JSX.Element;
+}
+
+const Body = styled.div`
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default function Layout(props: ILayoutProps) {
+  return (
+    <>
+      <LayoutHeader></LayoutHeader>
+      <LayoutBanner></LayoutBanner>
+      <LayoutNavigation></LayoutNavigation>
+      <Body>{props.children}</Body>
+    </>
+  );
+}
