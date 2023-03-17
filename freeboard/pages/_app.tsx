2830a72733ac263ad640e7066f71c589f31fdc06
DevLJW <jaewookv123@gmail.com>
2023-03-17 14:20:53 +0900

    apollo setting add

--- freeboard/pages/_app.tsx
+++ freeboard/pages/_app.tsx
@@ -1,15 +1,15 @@
 import "../styles/globals.css";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
 import type { AppProps } from "next/app";
+import ApolloSettingProps from "../src/components/apollo";
+import Layout from "../src/commons/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-      const client = new ApolloClient({
-            uri: "https://backendonline.codebootcamp.co.kr/graphql",
-            cache: new InMemoryCache(),
-      });
-      return (
-            <ApolloProvider client={client}>
-                  <Component {...pageProps} />
-            </ApolloProvider>
-      );
+  return (
+    <ApolloSettingProps>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloSettingProps>
+  );
 }
