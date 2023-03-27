710bad98d5c1f245947369240e911d8426cb9b02
DevLJW <jaewookv123@gmail.com>
2023-03-27 14:28:52 +0900

    commons layout update

--- freeboard/src/commons/layout/index.tsx
+++ freeboard/src/commons/layout/index.tsx
@@ -1,7 +1,9 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
 import LayoutBanner from "./banner/LayoutBanner.container";
 import LayoutHeader from "./header/LayoutHeader.container";
 import LayoutNavigation from "./navigation/LayoutNavigation.container";
+const HIDDEN = ["/12-02-library-star"];
 
 interface ILayoutProps {
   children: JSX.Element;
@@ -15,8 +17,12 @@ const Body = styled.div`
 `;
 
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+  const isHiddenHeader = HIDDEN.includes(router.asPath);
+
   return (
     <>
+      {/* false일때만 보여줘{!isHiddenHeader && <LayoutHeader></LayoutHeader>} */}
       <LayoutHeader></LayoutHeader>
       <LayoutBanner></LayoutBanner>
       <LayoutNavigation></LayoutNavigation>
