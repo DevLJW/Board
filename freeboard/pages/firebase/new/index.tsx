6c564ef310f5b7696e2330aafc8bbd4152dc91aa
DevLJW <jaewookv123@gmail.com>
2023-04-09 23:00:40 +0900

    FireBase Page Component add

---
+++ freeboard/pages/firebase/new/index.tsx
@@ -0,0 +1,5 @@
+import FireBaseBoardWrite from "../../../src/components/units/firebase/write/firebaseboardwrite.container";
+
+export default function FirebasePage() {
+  return <FireBaseBoardWrite></FireBaseBoardWrite>;
+}
