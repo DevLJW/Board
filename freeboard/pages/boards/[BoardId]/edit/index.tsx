fcec5d20c59666b8c7aa7ec498397848350b4493
DevLJW <jaewookv123@gmail.com>
2023-05-03 23:46:47 +0900

    Board Write Update Page Update

--- freeboard/pages/boards/[BoardId]/edit/index.tsx
+++ freeboard/pages/boards/[BoardId]/edit/index.tsx
@@ -4,32 +4,33 @@ import { gql, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 
 import {
-      IQuery,
-      IQueryFetchBoardArgs,
+  IQuery,
+  IQueryFetchBoardArgs,
 } from "../../../../src/commons/types/generated/types";
 
 const FETCH_BOARD = gql`
-      query fetchBoard($boardId: ID!) {
-            fetchBoard(boardId: $boardId) {
-                  writer
-                  title
-                  contents
-                  youtubeUrl
-                  boardAddress {
-                        zipcode
-                        address
-                        addressDetail
-                  }
-            }
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      writer
+      title
+      contents
+      youtubeUrl
+      boardAddress {
+        zipcode
+        address
+        addressDetail
       }
+      images
+    }
+  }
 `;
 
 export default function BoardWritePage() {
-      const router = useRouter();
-      const { data } = useQuery<
-            Pick<IQuery, "fetchBoard">,
-            IQueryFetchBoardArgs
-      >(FETCH_BOARD, { variables: { boardId: String(router.query.BoardId) } });
+  const router = useRouter();
+  const { data } = useQuery<Pick<IQuery, "fetchBoard">, IQueryFetchBoardArgs>(
+    FETCH_BOARD,
+    { variables: { boardId: String(router.query.BoardId) } }
+  );
 
-      return <BoardWrite isEdit={true} data={data} />;
+  return <BoardWrite isEdit={true} data={data} />;
 }
