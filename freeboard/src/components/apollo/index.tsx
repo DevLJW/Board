4c71a1478b0d73b8b05063200c8b40187a0993df
DevLJW <jaewookv123@gmail.com>
2023-05-05 22:08:25 +0900

    apollo uploadlink add

--- freeboard/src/components/apollo/index.tsx
+++ freeboard/src/components/apollo/index.tsx
@@ -1,11 +1,18 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloLink } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 export default function ApolloSettingProps(props: IApolloSettingProps) {
+  const uploadLink = createUploadLink({
+    uri: "https://backendonline.codebootcamp.co.kr/graphql",
+  });
+
   const client = new ApolloClient({
     uri: "https://backendonline.codebootcamp.co.kr/graphql",
+    link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
     cache: new InMemoryCache(),
   });
 
