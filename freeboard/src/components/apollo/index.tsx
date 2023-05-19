b0f05de32a5a4c64b29b8cf0f7fa7f7b828223d9
DevLJW <jaewookv123@gmail.com>
2023-05-19 15:18:44 +0900

    Memory Cache Global Update

--- freeboard/src/components/apollo/index.tsx
+++ freeboard/src/components/apollo/index.tsx
@@ -2,6 +2,9 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ApolloLink } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
+const GLOBAL_STATE = new InMemoryCache(); //  InMemoryCache() 아폴로 전역캐시를 저장해놓는곳
+//  페이지 이동시 app.tsx부터 리렌더링되어 지역변수가 아닌 전역으로 빼준다.
+
 interface IApolloSettingProps {
   children: JSX.Element;
 }
@@ -13,7 +16,7 @@ export default function ApolloSettingProps(props: IApolloSettingProps) {
   const client = new ApolloClient({
     uri: "https://backendonline.codebootcamp.co.kr/graphql",
     link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
-    cache: new InMemoryCache(),
+    cache: GLOBAL_STATE,
   });
 
   //    prettier-ignore
