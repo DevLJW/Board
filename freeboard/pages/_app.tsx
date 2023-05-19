2a20322a6ea459c4e01f7d3c0d43f6afa6df9c9f
DevLJW <jaewookv123@gmail.com>
2023-05-19 15:19:26 +0900

    Recoil Setting Add

--- freeboard/pages/_app.tsx
+++ freeboard/pages/_app.tsx
@@ -3,13 +3,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import ApolloSettingProps from "../src/components/apollo";
 import Layout from "../src/commons/layout";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloSettingProps>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloSettingProps>
+    //  내부 컴포넌트 리코일 모두사용
+    <RecoilRoot>
+      <ApolloSettingProps>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloSettingProps>
+    </RecoilRoot>
   );
 }
