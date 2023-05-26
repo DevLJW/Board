7a08e206a75fe3401b2664789a03a33b857d802f
DevLJW <jaewookv123@gmail.com>
2023-05-26 17:19:34 +0900

    Board List container Update

--- freeboard/src/components/units/board/list/BoardList.container.tsx
+++ freeboard/src/components/units/board/list/BoardList.container.tsx
@@ -6,12 +6,28 @@ import {
   IQueryFetchBoardsCountArgs,
 } from "../../../../commons/types/generated/types";
 import BoardListUI from "./BoardList.presenter";
-import { FETCH_BOARDS, FETCH_BOARDS_COUNT } from "./BoardListqueries";
-import { MouseEvent, useState } from "react";
+import {
+  FETCH_BOARDS,
+  FETCH_BOARDS_COUNT,
+  FETCH_USER_LOGGED_IN,
+} from "./BoardListqueries";
+import { MouseEvent, useEffect, useState } from "react";
+import { accessTokenState } from "../../../../commons/store";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 export default function BoardListPage() {
+  //const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  const { data: LoginUser } =
+    useQuery<Pick<IQuery, "fetchUserLoggedIn">>(FETCH_USER_LOGGED_IN);
+  console.log(LoginUser);
+
   const router = useRouter();
   const [keyword, setChangeKeyword] = useState("");
+
+  // 로그인한 사용자 정보 가져오기
+
+  //  프리렌더링 과정에서 프론트엔드는 localStorage 기능이 없기때문에 렌더링이 끝난 후 실행되는 useEffetc사용
 
   //  게시글 리스트 출력 쿼리 + 리패치쿼리 하나의 쿼리로 2개기능 사용
   const { data, refetch } = useQuery<
@@ -19,14 +35,12 @@ export default function BoardListPage() {
     IQueryFetchBoardsArgs
   >(FETCH_BOARDS); //바로 실행 후, 조회한 값을 data에 반환
 
-  console.log(data);
-
   //  작성된 게시글 총 개수 쿼리
   const { data: BoardsCount, refetch: refetchBoardsCount } = useQuery<
     Pick<IQuery, "fetchBoardsCount">,
     IQueryFetchBoardsCountArgs
-  >(FETCH_BOARDS_COUNT); //바로 실행 후, 조회한 값을 data에 반환
-  console.log(BoardsCount);
+  >(FETCH_BOARDS_COUNT);
+  //바로 실행 후, 조회한 값을 data에 반환
 
   //useQuery실행중 데이터가 다 안가져왔으면 안가져온 채로 넘기고 다가져와지면 다시 data에 값이 들어감
   // 화면에 보여줄때 조건부 렌더링? 달아서 보여주기 안가져온채면 아직안보여주고 가져오고 나서 보여줌
