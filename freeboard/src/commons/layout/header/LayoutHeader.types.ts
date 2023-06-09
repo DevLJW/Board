92b7b445cfd7b262da306af14c80e9baccd49660
DevLJW <jaewookv123@gmail.com>
2023-06-09 16:53:04 +0900

    LayoutHeader Types add

--- freeboard/src/commons/layout/header/LayoutHeader.types.ts
+++ freeboard/src/commons/layout/header/LayoutHeader.types.ts
@@ -2,4 +2,5 @@ export interface ILayoutHeaderUI {
   onClickLogo: () => void;
   onClickMoveToLogin: () => void;
   onClickMoveToUserJoin: () => void;
+  onClickMoveToShoppingBaskets: () => void;
 }
