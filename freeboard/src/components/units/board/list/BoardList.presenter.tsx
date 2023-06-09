a2014c37f2cce2e96c2be2772284d50bccc7e85a
DevLJW <jaewookv123@gmail.com>
2023-06-09 16:53:33 +0900

    BoardList Presenter Update

--- freeboard/src/components/units/board/list/BoardList.presenter.tsx
+++ freeboard/src/components/units/board/list/BoardList.presenter.tsx
@@ -19,38 +19,69 @@ export default function BoardListUI(props: IBoardListUIProps) {
         <S.ColumnHeaderTitle>제목</S.ColumnHeaderTitle>
         <S.ColumnHeaderBasic>작성자</S.ColumnHeaderBasic>
         <S.ColumnHeaderBasic>날짜</S.ColumnHeaderBasic>
+        {props.isBoolean ? <S.ColumnBuy>장바구니담기</S.ColumnBuy> : ""}
       </S.Row>
-      {props.data?.fetchBoards.map((el, index) => (
-        <S.Row key={el._id}>
-          <S.ColumnBasic>{index + 1}</S.ColumnBasic>
-          {
-            /* S.ColumTitle은 제목 전체영역 전체영역안에 S.TextToken이 있어 TextToken이 눌릴때 ColumTitle에 있는 온클릭이 실행 되어*/
-            // S.ColumnTitle의 아이디 값을 가져와야하므로 event.targetCurrent.id를 사용
-          }
-          <S.ColumnTitle id={el._id} onClick={props.onClickBoardDetail}>
-            {el.title
-              .replaceAll(props.keyword, `!@#${props.keyword}!@#`)
-              .split("!@#") //  !@#안녕하세요!@# --> [!@#,안녕하세요,!@#]
-              .map((el) => (
-                <S.TextToken key={uuid4()} isMatched={props.keyword === el}>
-                  {el}
-                </S.TextToken>
-              ))}
-          </S.ColumnTitle>
-          <S.ColumnBasic>{el.writer}</S.ColumnBasic>
-          <S.ColumnBasic>{getMyDate(el.createdAt)}</S.ColumnBasic>
-        </S.Row>
-      ))}
+
+      {props.isBoolean
+        ? props.data?.fetchBoards.map((el, index) => (
+            <S.Row key={el._id}>
+              <S.ColumnBasic>{index + 1}</S.ColumnBasic>
+              {
+                /* S.ColumTitle은 제목 전체영역 전체영역안에 S.TextToken이 있어 TextToken이 눌릴때 ColumTitle에 있는 온클릭이 실행 되어*/
+                // S.ColumnTitle의 아이디 값을 가져와야하므로 event.targetCurrent.id를 사용
+              }
+              <S.ColumnTitle id={el._id} onClick={props.onClickBoardDetail}>
+                {el.title
+                  .replaceAll(props.keyword, `!@#${props.keyword}!@#`)
+                  .split("!@#") //  !@#안녕하세요!@# --> [!@#,안녕하세요,!@#]
+                  .map((el) => (
+                    <S.TextToken key={uuid4()} isMatched={props.keyword === el}>
+                      {el}
+                    </S.TextToken>
+                  ))}
+              </S.ColumnTitle>
+              <S.ColumnBasic>{el.writer}</S.ColumnBasic>
+              <S.ColumnBasic>{getMyDate(el.createdAt)}</S.ColumnBasic>
+              <S.ColumnBuy onClick={props.onClickBasket(el)}>+</S.ColumnBuy>
+            </S.Row>
+          ))
+        : props.MyPageBaskets.map((el, index) => (
+            <S.Row key={el._id}>
+              <S.ColumnBasic>{index + 1}</S.ColumnBasic>
+              {
+                /* S.ColumTitle은 제목 전체영역 전체영역안에 S.TextToken이 있어 TextToken이 눌릴때 ColumTitle에 있는 온클릭이 실행 되어*/
+                // S.ColumnTitle의 아이디 값을 가져와야하므로 event.targetCurrent.id를 사용
+              }
+              <S.ColumnTitle id={el._id} onClick={props.onClickBoardDetail}>
+                {el.title
+                  .replaceAll(props.keyword, `!@#${props.keyword}!@#`)
+                  .split("!@#") //  !@#안녕하세요!@# --> [!@#,안녕하세요,!@#]
+                  .map((el) => (
+                    <S.TextToken key={uuid4()} isMatched={props.keyword === el}>
+                      {el}
+                    </S.TextToken>
+                  ))}
+              </S.ColumnTitle>
+              <S.ColumnBasic>{el.writer}</S.ColumnBasic>
+              <S.ColumnBasic>{getMyDate(el.createdAt)}</S.ColumnBasic>
+            </S.Row>
+          ))}
       <S.TableBottom />
       <S.Footer>
-        <PaginationsMain
-          refetch={props.refetch}
-          BoardsCount={props.BoardsCount}
-        ></PaginationsMain>
-        <S.Button onClick={props.onClickNewBoard}>
-          <S.PencilIcon src="/images/board/list/write.png" />
-          게시물 등록하기
-        </S.Button>
+        {props.isBoolean ? (
+          <>
+            <PaginationsMain
+              refetch={props.refetch}
+              BoardsCount={props.BoardsCount}
+            ></PaginationsMain>
+            <S.Button onClick={props.onClickNewBoard}>
+              <S.PencilIcon src="/images/board/list/write.png" />
+              게시물 등록하기
+            </S.Button>{" "}
+          </>
+        ) : (
+          ""
+        )}
       </S.Footer>
     </S.Wrapper>
   );
