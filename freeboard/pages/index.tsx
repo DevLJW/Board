21a7a70dd4f87f1c6b4e0eb6bbdc88cc0b0be103
DevLJW <jaewookv123@gmail.com>
2023-06-27 22:39:01 +0900

    pages index Update

--- freeboard/pages/index.tsx
+++ freeboard/pages/index.tsx
@@ -1,5 +1,8 @@
+import { WithAuth } from "../src/commons/hocs/withAuth";
 import BoardListPage from "../src/components/units/board/list/BoardList.container";
 
-export default function BoardsPage() {
-      return <BoardListPage></BoardListPage>;
+function BoardsPage() {
+  return <BoardListPage isBoolean={true}></BoardListPage>;
 }
+
+export default WithAuth(BoardsPage); //  app.tsx Component에 묶어서 전달
