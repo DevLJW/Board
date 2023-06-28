8b4c3397a630d4350c3f370a98928ca6832c2f5d
DevLJW <jaewookv123@gmail.com>
2023-06-28 19:04:29 +0900

    UserJoin index Update

---
+++ freeboard/pages/UserJoin/index.tsx
@@ -0,0 +1,5 @@
+import JoinLogin from "../../src/components/units/User/Join_Login/Join_Login.container";
+
+export default function UserJoinPage() {
+  return <JoinLogin isEdit={false}></JoinLogin>;
+}
