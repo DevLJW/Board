9822ae7fc32d89d650b0cdaee56b363a235e4841
DevLJW <jaewookv123@gmail.com>
2023-07-25 21:11:23 +0900

    Join Login queries add

---
+++ freeboard/src/components/units/User/Join_Login/Join_Login.queries.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const JOIN_USER = gql`
+  mutation createUser($createUserInput: CreateUserInput!) {
+    createUser(createUserInput: $createUserInput) {
+      _id
+      email
+      name
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
