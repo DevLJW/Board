6f0dfaaae95598b6f222555d4b9dbdcd23adaa9a
DevLJW <jaewookv123@gmail.com>
2023-07-25 21:10:45 +0900

    Join Login styles add

---
+++ freeboard/src/components/units/User/Join_Login/Join_Login.styles.ts
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import styled from "@emotion/styled";
+
+export const ButtonJoinLogin = styled(Button)``;
+
+export const CheckboxJoinLogin = styled(Checkbox)``;
+
+export const FormJoinLogin = styled(Form)`
+  width: 800px;
+  margin-top: 100px;
+`;
+
+export const FormItemJoinLogin = styled(Form.Item)``;
+
+export const InputJoinLogin = styled(Input)``;
+
+export const InputPasswordJoinLogin = styled(Input.Password)``;
