588de4b985f2ce3f5fd2bf08402b5aa9a8d2f123
DevLJW <jaewookv123@gmail.com>
2023-08-20 23:59:36 +0900

    LayoutHeader Upadate

--- freeboard/src/commons/layout/header/LayoutHeader.presenter.tsx
+++ freeboard/src/commons/layout/header/LayoutHeader.presenter.tsx
@@ -6,15 +6,27 @@ import {
 } from "./LayoutHeader.styles";
 import { ILayoutHeaderUI } from "./LayoutHeader.types";
 
+const exam = () => {
+  console.log("개발예정");
+};
+
 export default function LayoutHeaderUI(props: ILayoutHeaderUI) {
   return (
     <Wrapper>
       <InnerWrapper>
         <InnerLogo onClick={props.onClickLogo}>💎 LIVE</InnerLogo>
         <div>
-          <InnerButton onClick={props.onClickMoveToLogin}>로그인</InnerButton>
-          <InnerButton onClick={props.onClickMoveToUserJoin}>
-            회원가입
+          <InnerButton
+            onClick={props.UserInfoData ? exam : props.onClickMoveToLogin}
+          >
+            {props.UserInfoData
+              ? props.UserInfoData.fetchUserLoggedIn.name
+              : "로그인"}
+          </InnerButton>
+          <InnerButton
+            onClick={props.UserInfoData ? exam : props.onClickMoveToUserJoin}
+          >
+            {props.UserInfoData ? "로그아웃" : "회원가입"}
           </InnerButton>
           <InnerButton onClick={props.onClickMoveToShoppingBaskets}>
             장바구니
