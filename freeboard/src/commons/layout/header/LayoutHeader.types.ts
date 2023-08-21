5ec3898ca307e4b67ae2f770c48fafd2d71928ca
DevLJW <jaewookv123@gmail.com>
2023-08-21 22:08:53 +0900

    LayoutHeader types add

--- freeboard/src/commons/layout/header/LayoutHeader.types.ts
+++ freeboard/src/commons/layout/header/LayoutHeader.types.ts
@@ -1,6 +1,9 @@
+import { IQuery } from "../../types/generated/types";
+
 export interface ILayoutHeaderUI {
   onClickLogo: () => void;
   onClickMoveToLogin: () => void;
   onClickMoveToUserJoin: () => void;
   onClickMoveToShoppingBaskets: () => void;
+  UserInfoData: Pick<IQuery, "fetchUserLoggedIn"> | undefined;
 }
