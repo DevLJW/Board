12407863fad580e54bb4638cef621c4ada5dd5b7
DevLJW <jaewookv123@gmail.com>
2023-08-21 22:09:55 +0900

    Layout Nav Update

--- freeboard/src/commons/layout/navigation/LayoutNavigation.presenter.tsx
+++ freeboard/src/commons/layout/navigation/LayoutNavigation.presenter.tsx
@@ -4,8 +4,8 @@ import ILayoutNavigationUI from "./LayoutNavigation.types";
 
 export default function LayoutNavigationUI(props: ILayoutNavigationUI) {
   const item = [
-    { name: "라이브 게시판", page: "/API/Image" },
-    { name: "라이브 아이템 ", page: "/markets" },
+    { name: "자유게시판", page: "/" },
+    { name: "중고마켓 ", page: "/UsedMarket" },
     { name: "마이페이지", page: "/mypages" },
   ];
   return (
