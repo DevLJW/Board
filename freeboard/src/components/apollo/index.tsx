200b60a7d152985beddadbd1687ed8e0bc05e297
DevLJW <jaewookv123@gmail.com>
2023-10-11 14:55:26 +0900

    apollo index update

--- freeboard/src/components/apollo/index.tsx
+++ freeboard/src/components/apollo/index.tsx
@@ -4,7 +4,8 @@ import { createUploadLink } from "apollo-upload-client";
 import { accessTokenState } from "../../commons/store";
 import { useRecoilState } from "recoil";
 import { useEffect } from "react";
-
+import { Global } from "@emotion/react";
+import { globalStyles } from "../../../styles/globalstyle";
 const GLOBAL_STATE = new InMemoryCache(); //  InMemoryCache() 아폴로 전역캐시를 저장해놓는곳
 //  페이지 이동시 app.tsx부터 리렌더링되어 지역변수가 아닌 전역으로 빼준다.
 
@@ -21,6 +22,7 @@ export default function ApolloSettingProps(props: IApolloSettingProps) {
       if (result) setAccessToken(result);
     }
   }, []);
+
   const uploadLink = createUploadLink({
     uri: "https://backendonline.codebootcamp.co.kr/graphql",
     headers: { Authorization: `Bearer ${accessToken}` },
@@ -34,6 +36,7 @@ export default function ApolloSettingProps(props: IApolloSettingProps) {
 
   //    prettier-ignore
   return <ApolloProvider client={client}>
+      <Global styles={globalStyles}></Global>
     {props.children}
 
   </ApolloProvider>;
